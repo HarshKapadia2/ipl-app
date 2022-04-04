e769fd50395e23ce01a32d374657ff68b756c989
Harsh Kapadia <harshgkapadia@gmail.com>
2022-04-05 02:28:33 +0530

    :truck: update: Get backend ready for deployment
    Add error checking

--- server/main.py
+++ server/main.py
@@ -1,24 +1,57 @@
 from flask import Flask, jsonify, render_template
 from flask import request
-from flask_cors import CORS
+import json
 from mlr import make_recommendation
 
-
 app = Flask(__name__)
-CORS(app)
 
+@app.route("/", methods = ["GET"])
+def homePage():
+	return render_template("index.html")
 
-@app.route("/get")
-def get_price():
-    t1 = float(request.args.get("raa"))
-    t2 = float(request.args.get("wins"))
-    t3 = float(request.args.get("efscore"))
-    t4 = float(request.args.get("salary"))
-    searchTerms = [t1, t2, t3, t4]
-    print(searchTerms)
-    res = make_recommendation(searchTerms)
-    print(res)
-    return str(res)
+@app.route("/predict", methods = ["POST", "OPTIONS"])
+def predictPrice():
+	if request.method == "OPTIONS": # For the CORS Preflight request
+		return corsPreflightResponse()
+	elif request.method == "POST":
+		try:
+			data = request.json
+		except:
+			return jsonify({"errorMsg": "Malformed request body."}), 400
 
+		raa = data["raa"]
+		wins = data["wins"]
+		efScore = data["ef-score"]
+		salary = data["salary"]
 
-app.run(debug=True)
+		if not isinstance(raa, int) and not isinstance(raa, float):
+			return jsonify({"errorMsg": "RAA should be a number."}), 400
+		if not isinstance(wins, int) and not isinstance(wins, float):
+			return jsonify({"errorMsg": "Wins should be a number."}), 400
+		if not isinstance(efScore, int) and not isinstance(efScore, float):
+			return jsonify({"errorMsg": "EF score should be a number."}), 400
+		if not isinstance(salary, int) and not isinstance(salary, float):
+			return jsonify({"errorMsg": "Salary should be a number."}), 400
+
+		if efScore < 0 or efScore > 1:
+			return jsonify({"errorMsg": "EF score should be between 0 and 1, both inclusive."}), 400
+
+		inputs = [raa, wins, efScore, salary]
+		predictedPrice = make_recommendation(inputs)
+
+		response = jsonify({"predictedPrice": predictedPrice})
+		# response.headers.add("Access-Control-Allow-Origin", "https://harshkapadia2.github.io")
+		response.headers.add("Access-Control-Allow-Origin", "*")
+
+		return response, 200
+
+def corsPreflightResponse():
+	response = make_response()
+	# response.headers.add("Access-Control-Allow-Origin", "https://harshkapadia2.github.io")
+	response.headers.add("Access-Control-Allow-Origin", "*")
+	response.headers.add('Access-Control-Allow-Headers', "*")
+	response.headers.add('Access-Control-Allow-Methods', "POST")
+	return response
+
+if __name__ == "__main__":
+	app.run(debug = True)
